286ac4ab86e920b1ba7ad24deb678682699dcc99
Z-Pelczar <zygmip12@gmail.com>
2021-07-02 10:50:34 +0200

    zrobione

--- main.py
+++ main.py
@@ -20,11 +20,14 @@ generation = 0
 class Car(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.body_image = pygame.image.load('assets/Car.png').convert_alpha()
+        self.image = pygame.image.load("assets/Car_red_lite.png")
+        self.image = pygame.transform.scale(self.image,(120,120))
+        self.body_image = self.image.convert_alpha()
         self.body_mask = pygame.mask.from_surface(self.body_image)
         self.body = self.body_image.get_rect()
+        self.rect = self.body_image.get_rect()
 
-
+        self.to_delete = False
 
         self.pozycja = [100,500]
         self.width = 165
@@ -36,6 +39,11 @@ class Car(pygame.sprite.Sprite):
 
         self.num = 0
 
+        self.numver_of_moves = 0
+
+        self.na_parkingu = False
+        self.na_lini = False
+        self.distance_b = 0
 
     def draw(self, win):
         self.blitRotateCenter(win,self.body_image,self.pozycja,self.angle)
@@ -44,6 +52,7 @@ class Car(pygame.sprite.Sprite):
     def update(self):
         self.pozycja[0] += math.cos(math.radians(360 - self.angle)) * self.speed
         self.pozycja[1] += math.sin(math.radians(360 - self.angle)) * self.speed
+        #self.rect.move(self.pozycja[0]-self.width/2,self.pozycja[1]-self.height/2)
 
 
 
@@ -58,28 +67,30 @@ class Car(pygame.sprite.Sprite):
         surf.blit(rotated_image, new_rect)
 
     def forward(self):
-        if self.speed < 20:
-            self.speed += 3
+        if self.speed < 10:
+            self.speed += 1
 
     def backward(self):
-        if self.speed > - 20:
-            self.speed -= 3
+        if self.speed > - 5:
+            self.speed -= 1
 
     def left(self):
-        self.angle += 0.4 * self.speed
+        if self.speed != 0:
+            self.angle += 15
 
     def right(self):
-        self.angle -= 0.4 * self.speed
+        if self.speed != 0:
+            self.angle += -15
 
-    def random_act(self):
-        act = random.randrange(0,6)
+    def random_act(self,act):
+        
         if act == 0:
             self.forward()
         elif act == 1:
             self.backward()
         elif act == 2:
             self.right()
-        else:
+        elif act == 3:
             self.left()
 
 ########################################################################################################################
@@ -122,13 +133,14 @@ class Border(pygame.sprite.Sprite):
 
 #klasa prize z pustym parkingiem, za który dostaje się nagrodę
 class Prize():
-    def __init__(self):
-        self.x = 1800
-        self.y = 0
-        self.h = 1000
-        self.w = 50
-        self.img = pygame.image.load("assets/Empty.png")
-        self.img = pygame.transform.scale(self.img, [self.w, self.h])
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.sprite = 'assets/Parking.png'
+        self.parking_image = pygame.image.load(self.sprite).convert_alpha()
+        self.parking_mask = pygame.mask.from_surface(self.parking_image)
+        self.parking_body = self.parking_image.get_rect()
+        self.naparkingu = False
 
     def draw(self, win):
         win.blit(self.img, (self.x, self.y))
@@ -136,6 +148,19 @@ class Prize():
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
+
+class Parkingline(pygame.sprite.Sprite):
+    def __init__(self, img, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.img = img
+        self.parkline_image = pygame.image.load(self.img).convert_alpha()
+        self.parkline_mask = pygame.mask.from_surface(self.parkline_image)
+        self.parkline_body = self.parkline_image.get_rect()
+        self.nalinii = False
+############################################################################
+
 
 #rysujemy auta itp
 def draw_window(win, cars, obstacles):
@@ -149,10 +174,24 @@ def draw_window(win, cars, obstacles):
         win.blit(obstacle.obstacle_image, (obstacle.x, obstacle.y))
 
 
-def eval_genomes():
+def eval_genomes(genomes, config):
+    global generation
+
+    distance = math.sqrt((160 - 856) ** 2 + (560 - 774) ** 2)
 
     cars = []
-    cars.append(Car())
+    ge = []
+    nets = []
+    for genome_id, genome in genomes:
+        genome.fitness = 0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+        cars.append(Car())
+        ge.append(genome)
+
+    for car in cars:
+        car.distance_b = distance
+
 
     parked_car_placement = [[390, 145],
                             [535, 145],
@@ -164,8 +203,8 @@ def eval_genomes():
                             [1390, 145],
                             [1540, 145],
                             [390, 695],
+                            [535, 695],
                             [675, 695],
-                            [820, 695],
                             [965, 695],
                             [1110, 695],
                             [1250, 695],
@@ -186,6 +225,18 @@ def eval_genomes():
                         [0, 868],
                         [0, 706]]
 
+    parking_lines_sprites = [['assets/parking_line.png'],
+                             ['assets/parking_line2.png'],
+                             ['assets/parking_line3.png']]
+
+    parking_lines_cords = [[766, 671],
+                           [914, 671],
+                           [762, 653]]
+
+    parking_lines = []
+    for n, x in enumerate(parking_lines_cords):
+        parking_lines.append(Parkingline(parking_lines_sprites[n][0], x[0], x[1]))
+
     obstacles = []
     for x in parked_car_placement:
         obstacles.append(Obstacle(x[0], x[1]))
@@ -193,11 +244,14 @@ def eval_genomes():
     for n, x in enumerate(border_placement):
         obstacles.append(Border(border_sprites[n][0], x[0], x[1]))
 
+    parking = Prize(800, 675)
+
     pygame.init()
     screen = pygame.display.set_mode((1900, 1000))
+    screen.blit(parking.parking_image, (parking.x, parking.y))
     map = pygame.image.load('assets/Map.png')
     act_time = 0
-
+    generation += 1
 
     clock = pygame.time.Clock()
     run = True
@@ -213,10 +267,55 @@ def eval_genomes():
                 quit()
                 break
 
-        if act_time == 5:
+
+
+
+
+
+
+
+
+        if act_time == 3:
             act_time = 0
-            for car in cars:
-                car.random_act()
+            for x, car in enumerate(cars):
+                prize = car.distance_b - car.distance(856,774)
+                ge[x].fitness += prize * 3
+                car.distance_b = car.distance(856,774)
+                car.numver_of_moves += 1
+
+
+
+                #inputs = [car.speed,car.angle,car.pozycja[0],car.pozycja[1],car.distance(520,700)]
+
+                output = nets[cars.index(car)].activate((car.speed,car.angle,car.pozycja[0],car.pozycja[1],car.distance(856,774),car.distance(710,774),car.distance(1000,774)))
+                i = output.index(max(output))
+
+
+                if i == 0:
+
+                    car.forward()
+                elif i == 1:
+                    car.backward()
+                elif i == 2:
+                    car.left()
+                elif i ==3 :
+                    car.right()
+
+                for parking_line in parking_lines:
+                    screen.blit(parking_line.parkline_image, (parking_line.x, parking_line.y))
+                    offset = (int(car.pozycja[0]- parking_line.x), int(car.pozycja[1] - parking_line.y))
+                    result = parking_line.parkline_mask.overlap(car.body_mask, offset)
+                    if result:
+                        car.na_lini = True
+                    else:
+                        car.na_lini = False
+
+                offset = (int(car.pozycja[0] - parking.x), int(car.pozycja[1] - parking.y))
+                result = parking.parking_mask.overlap(car.body_mask, offset)
+                if result:
+                    car.na_parkingu = True
+                else:
+                    car.na_parkingu= False
 
 
 
@@ -224,8 +323,24 @@ def eval_genomes():
                     offset = (int(car.pozycja[0] - obs.x),int(car.pozycja[1] - obs.y))
                     result = obs.obstacle_mask.overlap(car.body_mask, offset)
                     if result:
-                        print(car.num)
-                        car.num = car.num + 1
+                        car.to_delete = True
+                        ge[x].fitness -= -50
+
+                if car.na_parkingu and not car.na_lini:
+                    ge[x].fitness += 500
+
+
+
+                if car.numver_of_moves > 100:
+                    car.to_delete = True
+                    ge[x].fitness -= 500
+
+                if car.to_delete:
+                    nets.pop(cars.index(car))
+                    ge.pop(cars.index(car))
+                    cars.pop(cars.index(car))
+
+
         for car in cars:
             car.update()
         screen.blit(map, (0, 0))
@@ -252,5 +367,7 @@ def run(config_file):
 
 if __name__ == '__main__':
     local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, 'config-feedforward.txt')
+    config_path = os.path.join(local_dir, 'config_feedforward.txt')
     run(config_path)
+
+
