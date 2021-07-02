e79a6a20901162d8598d3025f0f536aa9b9bf046
Lordi255 <mlenergetyk@gmail.com>
2021-07-02 03:50:16 +0200

    pygame pixel perfect

--- Game.py
+++ Game.py
@@ -1,230 +1,32 @@
-import arcade
-import Car
-import Parking
+import pygame
 
+bg_img = pygame.image.load("assets/Map.png")
 
-import random
+win_width = 1900
+win_heigth = 1000
+backround = pygame.image.load("assets/Map.png")
+WIN = pygame.display.set_mode((win_width, win_heigth))
 
+class game():
+    def __init__(self):
+        pass
 
+def run():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(60)
 
-class Game(arcade.Window):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+                break
 
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title)
+        window = WIN
+        window.blit(backround, [0, 0])
 
-        self.parking = Parking.Parking()
+        pygame.display.flip()
+        clock.tick(0)
 
-
-        #widok hitboxów
-        self.hitbox_visible = True
-
-        # Zmienne mówiące o tym czy dany klawisz jest wciśnięty.
-        self.pressed_up = False
-        self.pressed_down = False
-        self.pressed_right = False
-        self.pressed_left = False
-
-        # Zmienne dotyczące tworzenia zaparokwanych samochodów.
-        self.random_car_placement = True
-        self.random_car_number = True
-        self.parked_car_number = 15
-
-        # Pozycje zaparkowanych samochodów, pierwsza wartość oznacz miejscie (liczone od lewa do prawa zaczynając od
-        # dołu, druga pozycje x, trzecia pozycje y.
-        self.spawn_points = [(1,430,230),(2,572,230),(3,714,230),(4,856,230),(5,998,230),(6,1140,230),(7,1282,230),
-                             (8,1424,230),(9,1566,230),(10,430,780),(11,572,780),(12,714,780),(13,856,780),(14,998,780),
-                             (15,1140,780),(16,1282,780),(17,1424,780),(18,1566,780)]
-
-        arcade.set_background_color(arcade.color.AMAZON)
-
-        self.setup()
-        arcade.run()
-
-        # If you have sprite lists, you should create them here,
-        # and set them to None
-
-        self.player_car_list = None
-        self.car_sprite = None
-
-        self.parked_car_list = None
-        self.parked_car_sprite = None
-        self.parking_slot_list = None
-        self.parking_line_list = None
-        self.parking_block_list = None
-        self.parking_slot_border_list = None
-
-		# Pozycje zaparkowanych samochodów, pierwsza wartość oznacz miejscie (liczone od lewa do prawa zaczynając od
-        # dołu), druga pozycje x, trzecia pozycje y.
-        self.spawn_points = [(1, 430, 230), (2, 572, 230), (3, 714, 230), (4, 856, 230), (5, 998, 230), (6, 1140, 230),
-                             (7, 1282, 230), (8, 1424, 230), (9, 1566, 230),
-                             (10, 430, 780), (11, 572, 780), (12, 714, 780), (13, 856, 780), (14, 998, 780),
-                             (15, 1140, 780), (16, 1282, 780), (17, 1424, 780), (18, 1566, 780)]
-
-    def car_spawn(self):
-        """
-        Funkcja odpowiadająca za utrzworzenie zaparkowanych samochodów, zależna od: "random_car_placement" mówiące czy
-        ustawienie samochodów ma być losowe czy wcześniej z góry ustalone, "random_car_number" mówiąące czy liczba
-        samochodów ma być losowa czy z góry ustalona, "parked_car_number" mówiące ile samochodów ma zostać utworzonych.
-        """
-        if self.random_car_placement:
-            if self.random_car_number:
-                cars_to_spawn = random.sample(self.spawn_points,random.randrange(0,len(self.spawn_points)-1))
-            else:
-                cars_to_spawn = random.sample(self.spawn_points,self.parked_car_number)
-        else:
-            cars_to_spawn = []
-            for spawn_point in self.spawn_points:
-                if spawn_point[0] in [3,9,11,16]:
-                    pass
-                else:
-                    cars_to_spawn.append(spawn_point)
-        for spawn_point in cars_to_spawn:
-            number = random.randrange(1, 10)
-            self.parked_car_sprite = Car.Car(f"assets/Car{number}.png", 0.15)
-            self.parked_car_sprite.center_x = spawn_point[1]
-            self.parked_car_sprite.center_y = spawn_point[2]
-            self.parked_car_list.append(self.parked_car_sprite)
-
-    def setup(self):
-        """ Set up the game variables. Call to re-start the game. """
-        # Create your sprites and sprite lists here
-
-        self.player_car_list = arcade.SpriteList()
-        self.parked_car_list = arcade.SpriteList()
-        self.parking_slot_list = arcade.SpriteList()
-        self.parking_line_list = arcade.SpriteList()
-        self.parking_block_list = arcade.SpriteList()
-        self.parking_slot_border_list = arcade.SpriteList()
-
-
-
-
-        self.background = arcade.load_texture("assets/Map.png")
-        self.car_sprite = Car.Car("assets/Car.png", 0.15)
-        self.car_sprite.center_x = 100
-        self.car_sprite.center_y = 500
-
-
-        self.car_spawn()
-
-        self.player_car_list.append(self.car_sprite)
-
-        self.parking.add_parking_lane(self.parking_line_list)
-        self.parking.add_parking_slot(self.parking_slot_list)
-        self.parking.add_parking_block(self.parking_block_list)
-        self.parking.add_slot_border(self.parking_slot_border_list)
-
-    def on_draw(self):
-        """
-        Render the screen.
-        """
-
-        # This command should happen before we start drawing. It will clear
-        # the screen to the background color, and erase what we drew last frame.
-
-        arcade.start_render()
-
-        arcade.draw_lrwh_rectangle_textured(0, 0, 1900, 1000, self.background)
-        self.player_car_list.draw()
-        self.parked_car_list.draw()
-        self.parking_line_list.draw()
-        self.parking_slot_list.draw()
-        self.parking_block_list.draw()
-        self.parking_slot_border_list.draw()
-
-        #rysuje hitbox pojzadu głównego
-        if self.hitbox_visible == True:
-            self.car_sprite.draw_hit_box(arcade.color.BLUE, 3)
-
-            for car in self.parked_car_list:
-                car.draw_hit_box(arcade.color.RED, 3)
-
-            for slot in self.parking_slot_list:
-                slot.draw_hit_box(arcade.color.GREEN,3)
-
-            for line in self.parking_line_list:
-                line.draw_hit_box(arcade.color.YELLOW,3)
-
-            for block in self.parking_block_list:
-                block.draw_hit_box(arcade.color.RED,3)
-
-            for border in self.parking_slot_border_list:
-                border.draw_hit_box(arcade.color.YELLOW,1)
-
-
-        # Call draw() on all your sprite lists below
-
-
-    def parking_method(self,car):
-        """
-        Metoda sprawdzająca czy samochód podany jako argument car jest poprawnie zaparkowany.
-        """
-        if arcade.check_for_collision_with_list(car,self.parking_slot_list) and not arcade.check_for_collision_with_list(car,self.parking_line_list) and not arcade.check_for_collision_with_list(car,self.parking_slot_border_list) and not car.is_parked:
-            car.is_parked = True
-            print("Parked succesfully")
-
-        if arcade.check_for_collision_with_list(car,self.parking_line_list) or arcade.check_for_collision_with_list(car,self.parking_slot_border_list):
-            car.is_parked = False
-
-    def collision_method(self, car):
-
-        if arcade.check_for_collision_with_list(car,self.parking_block_list) or arcade.check_for_collision_with_list(car, self.parked_car_list):
-            self.setup()
-            return True
-
-    def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
-
-        # Czynności podejmowane w zależności od wciśniętych klawiszy.
-        if self.pressed_up == True and self.pressed_down == False:
-            self.car_sprite.acceleration()
-        if self.pressed_up == False and self.pressed_down == False:
-            self.car_sprite.deacceleration()
-        if self.pressed_down == True and self.pressed_up == False and \
-                self.car_sprite.current_speed > self.car_sprite.car_stop:
-            self.car_sprite.car_break()
-            self.car_sprite.is_breaking = True
-        if self.pressed_down == False:
-            self.car_sprite.is_breaking = False
-        if self.pressed_down == True and self.car_sprite.is_breaking == False:
-            self.car_sprite.car_back()
-        if self.pressed_left == True and self.pressed_right == False:
-            self.car_sprite.left()
-        if self.pressed_right == True and self.pressed_left == False:
-            self.car_sprite.right()
-        if (self.pressed_right == False and self.pressed_left == False) or self.car_sprite.is_breaking == True:
-            self.car_sprite.stop_angle()
-
-        self.player_car_list.update()
-
-        self.parking_method(self.car_sprite)
-
-        if (self.collision_method(self.car_sprite)):
-            print("Działa")
-
-
-
-
-    def on_key_press(self, key, key_modifiers):
-        if key == arcade.key.UP:
-            self.pressed_up = True
-        if key == arcade.key.DOWN:
-            self.pressed_down = True
-        if key == arcade.key.RIGHT:
-            self.pressed_right = True
-        if key == arcade.key.LEFT:
-            self.pressed_left = True
-
-    def on_key_release(self, key, key_modifiers):
-        if key == arcade.key.UP:
-            self.pressed_up = False
-        if key == arcade.key.DOWN:
-            self.pressed_down = False
-        if key == arcade.key.RIGHT:
-            self.pressed_right = False
-        if key == arcade.key.LEFT:
-            self.pressed_left = False
--- main.py
+++ main.py
@@ -1,10 +1,176 @@
+import math
 
-from Game import Game
+import pygame
+import random
+import os
+import neat
 
-SCREEN_WIDTH = 1900
-SCREEN_HEIGHT = 1000
-SCREEN_TITLE = "Self Learning Car"
+WIN_WIDTH = 1900
+WIN_HEIGHT = 1000
+WIN = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
+pygame.display.set_caption("Test")
+generation = 0
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+#klasa Car
+class Car(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.body_image = pygame.image.load('assets/Car.png').convert_alpha()
+        self.body_mask = pygame.mask.from_surface(self.body_image)
+        self.body = self.body_image.get_rect()
+        self.angle = 2
+
+        self.num = 0
+
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+#klasa Obstacle, czyli wszystkie przeszkody(również granice parkingu)/zaparkowane samochody na parkingu
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.obstacle_image = pygame.image.load('assets/Car1.png').convert_alpha()
+        self.obstacle_mask = pygame.mask.from_surface(self.obstacle_image)
+        self.obstacle_body = self.obstacle_image.get_rect()
+
+    def draw(self, win):
+        win.blit(self.obstacle_image, (self.x, self.y))
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+class Border(pygame.sprite.Sprite):
+    def __init__(self, sprite, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.sprite = sprite
+        self.obstacle_image = pygame.image.load(self.sprite).convert_alpha()
+        self.obstacle_mask = pygame.mask.from_surface(self.obstacle_image)
+        self.obstacle_body = self.obstacle_image.get_rect()
+
+    def draw(self, win):
+        win.blit(self.obstacle_image, (self.x, self.y))
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+#klasa prize z pustym parkingiem, za który dostaje się nagrodę
+class Prize():
+    def __init__(self):
+        self.x = 1800
+        self.y = 0
+        self.h = 1000
+        self.w = 50
+        self.img = pygame.image.load("assets/Empty.png")
+        self.img = pygame.transform.scale(self.img, [self.w, self.h])
+
+    def draw(self, win):
+        win.blit(self.img, (self.x, self.y))
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+#rysujemy auta itp
+def draw_window(win, cars, obstacles):
+
+    #for car in cars:
+    car = cars[0]
+    mx, my = pygame.mouse.get_pos()
+    win.blit(car.body_image, (mx, my))
+
+    for obstacle in obstacles:
+        win.blit(obstacle.obstacle_image, (obstacle.x, obstacle.y))
+        offset = (mx - obstacle.x, my - obstacle.y)
+        result = obstacle.obstacle_mask.overlap(car.body_mask, offset)
+        if result:
+            print(car.num)
+            car.num = car.num + 1
+
+def eval_genomes():
+
+    cars = []
+    cars.append(Car())
+
+    parked_car_placement = [[390, 145],
+                            [535, 145],
+                            [675, 145],
+                            [820, 145],
+                            [965, 145],
+                            [1110, 145],
+                            [1250, 145],
+                            [1390, 145],
+                            [1540, 145],
+                            [390, 695],
+                            [675, 695],
+                            [820, 695],
+                            [965, 695],
+                            [1110, 695],
+                            [1250, 695],
+                            [1390, 695],
+                            [1540, 695]]
+
+    border_sprites = [['assets/Border1.png'],
+                      ['assets/Border2.png'],
+                      ['assets/Border3.png'],
+                      ['assets/Border4.png'],
+                      ['assets/Border5.png'],
+                      ['assets/Border6.png']]
+
+    border_placement = [[0, 0],
+                        [0, 0],
+                        [0, 0],
+                        [1804, 0],
+                        [0, 868],
+                        [0, 706]]
+
+    obstacles = []
+    for x in parked_car_placement:
+        obstacles.append(Obstacle(x[0], x[1]))
+
+    for n, x in enumerate(border_placement):
+        obstacles.append(Border(border_sprites[n][0], x[0], x[1]))
+
+    pygame.init()
+    screen = pygame.display.set_mode((1900, 1000))
+    map = pygame.image.load('assets/Map.png')
+
+    clock = pygame.time.Clock()
+    run = True
+    while run and len(cars) > 0:
+        clock.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+                break
+
+
+
+        screen.blit(map, (0, 0))
+        draw_window(screen, cars, obstacles)
+        pygame.display.flip()
+        clock.tick(0)
+
+def run():
+    eval_genomes()
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
 
 if __name__ == '__main__':
-    game = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-
+    run()
